cc308de1366205a38f24b59510d8fe6358f8be1f
dwi67 <dieter.wijngaards@bluewin.ch>
2017-05-01 16:47:20 +0200

    Correct build

--- jboss-fuse-6.3-admin/build.sh
+++ jboss-fuse-6.3-admin/build.sh
@@ -5,5 +5,5 @@ docker rmi --force=true ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_VERSION}
 docker build --force-rm=true --rm=true -t ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_VERSION} .
 echo =========================================================================
 echo Docker image is ready.  Try it out by running:
-echo     docker run -it -p 8181:8181 adesso/jboss-fuse-6.3-admin
+echo     docker run -it -p 8181:8181 dwi67/jboss-fuse-6.3-admin
 echo =========================================================================
